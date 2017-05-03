eb31e4c6f8139a3698d7b5bff97fe6371872164f
Luca Guidi <me@lucaguidi.com>
2017-05-03 12:41:34 +0200

    Prepare for v0.0.1

--- lib/hanami_utils_escape.rb
+++ lib/hanami_utils_escape.rb
@@ -1,11 +1,3 @@
-require "helix_runtime"
-
-begin
-  require "hanami_utils_escape/native"
-rescue LoadError
-  warn "Unable to load hanami_utils_escape/native. Please run `rake build`"
-end
-
 class HanamiUtilsEscape
   require "hanami_utils_escape/version"
 
@@ -26,27 +18,13 @@ class HanamiUtilsEscape
 
   private_constant(:HTML_CHARS)
 
-  # Escape HTML contents
-  #
-  # This MUST be used only for tag contents.
-  # Please use `html_attribute` for escaping HTML attributes.
+  # Pure Ruby implementation of HTML escape
   #
   # @param input [String] the input
+  # @return [String] an UTF-8 encoded string
   #
-  # @return [String] the escaped string
-  #
-  # @since 0.4.0
-  #
-  # @see https://www.owasp.org/index.php/XSS_%28Cross_Site_Scripting%29_Prevention_Cheat_Sheet OWASP XSS Cheat Sheet Rule #1
-  #
-  # @example Good practice
-  #   <div><%= Hanami::Utils::Escape.html('<script>alert(1);</script>') %></div>
-  #   <div>&lt;script&gt;alert(1);&lt;&#x2F;script&gt;</div>
-  #
-  # @example Bad practice
-  #   # WRONG Use Escape.html_attribute instead
-  #   <a title="<%= Hanami::Utils::Escape.html('...') %>">link</a>
-  def self.html_ruby(input)
+  # @since 0.0.1
+  def self.html(input)
     input  = encode(input)
     result = ""
 
@@ -57,13 +35,18 @@ class HanamiUtilsEscape
     result
   end
 
+  class << self
+    # @since 0.0.1
+    # @api private
+    alias html_ruby html
+  end
+
   # Encode the given string into UTF-8
   #
   # @param input [String] the input
-  #
   # @return [String] an UTF-8 encoded string
   #
-  # @since 0.4.0
+  # @since 0.0.1
   # @api private
   def self.encode(input)
     return "" if input.nil?
@@ -73,4 +56,12 @@ class HanamiUtilsEscape
   end
 
   private_class_method :encode
+end
+
+require "helix_runtime"
+
+begin
+  require "hanami_utils_escape/native"
+rescue LoadError
+  warn "Unable to load hanami_utils_escape/native. Please run `rake build`"
 end
